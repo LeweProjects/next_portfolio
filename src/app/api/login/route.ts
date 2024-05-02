3470a8776a0124766335120018414252d3c9d6a3
Marvell Ayon <loewiayon12@gmail.com>
2024-05-02 08:34:06 +0800

    route comment

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,11 +1,11 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
-    res.end('Byee')
+// export default function (req: NextApiRequest, res: NextApiResponse) {
+//     res.end('Byee')
     
-    //console.log("REQUEST", req.body.username);
+//     //console.log("REQUEST", req.body.username);
 
-    // res.setHeader('Set-Cookie',"program=true");
+//     // res.setHeader('Set-Cookie',"program=true");
     
-    res.json({status: "ok"})
-}
+//     res.json({status: "ok"})
+// }
